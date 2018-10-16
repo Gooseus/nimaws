b4c49b6b1f4e536d0679ea601055b52b82a7b7a4
teroz <terence.namusonge@gmail.com>
2018-10-16 09:02:10 +0200

    Changes to be committed: 	modified:   tests/tminio.nim

--- tests/tminio.nim
+++ tests/tminio.nim
@@ -2,7 +2,7 @@ import unittest,os,asyncdispatch,httpclient,md5,osproc,strutils
 
 import s3client
 
-suite "Test minio Endpoint":
+suite "Test Minio Endpoint":
 
   when not existsEnv("MINIO_ACCESS_ID") and not existsEnv("MINIO_ACCESS_SECRET"):
     echo "NO MINIO ENV set"
@@ -20,16 +20,14 @@ suite "Test minio Endpoint":
 
     test "List Buckets":
 
-      let res = waitFor client.list_buckets
+      let res = waitFor client.list_buckets()
       assert res.code == Http200
 
     test "List Objects":
-      client.httpClient.headers.clear()
       let res = waitFor client.list_objects(bucket)
       assert res.code == Http200
 
     test "Put Object":
-      client.httpClient.headers.clear()
       var
         path = "/files/passwd"
         payload = if fileExists(passwd): readFile(passwd) else: "some file content\nbla bla bla"
@@ -38,7 +36,6 @@ suite "Test minio Endpoint":
       assert res.code == Http200
 
     test "Get Object":
-      client.httpClient.headers.clear()
       var
         path = "/files/passwd"
         f: File
